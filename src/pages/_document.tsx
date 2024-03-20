41346c0a2ccd96dc4592d8a6d8734e66e7a309f8
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-03-20 21:47:00 +0530

    added img

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="/next.svg" />
-        <meta property="og:image" content="/next.svg" />
+        <meta
+          property="fc:frame:image"
+          content="https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.linkedin.com%2Fin%2Ffidalmathew&psig=AOvVaw0qXv9i-JQzrQjghzxvCT-E&ust=1711037773876000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLD42seeg4UDFQAAAAAdAAAAABAE"
+        />
+        <meta
+          property="og:image"
+          content="https://www.google.com/url?sa=i&url=https%3A%2F%2Fin.linkedin.com%2Fin%2Ffidalmathew&psig=AOvVaw0qXv9i-JQzrQjghzxvCT-E&ust=1711037773876000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLD42seeg4UDFQAAAAAdAAAAABAE"
+        />
         <meta property="fc:frame:button:1" content="Click here" />
         <meta property="fc:frame:post_url" content="https://www.google.com" />
         <meta property="fc:frame:input:text" content="Enter your Input" />
