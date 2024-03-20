bedb957a1c7752e57605d2f3c9cfbb27f7b2f520
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-03-20 21:51:05 +0530

    added img

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="/download.jpeg" />
+        <meta
+          property="fc:frame:image"
+          content="https://far-from-farcaster.vercel.app/download.jpeg"
+        />
         <meta property="og:image" content="/download.jpeg" />
         <meta property="fc:frame:button:1" content="Click here" />
         <meta property="fc:frame:post_url" content="https://www.google.com" />
